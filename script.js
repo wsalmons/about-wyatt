31aa2aa9fe5f315f80ca8d8980e2a7cddf19cbee
Brandon Huffman <bt_huffman@msn.com>
2019-01-14 21:28:08 -0700

    Updated work on the about me website, attempting to set up a video that will play when click on the Kobayashi Maru image.

---
+++ script.js
@@ -0,0 +1,6 @@
+// function playScenario() {
+// 	window.onLoad("https://www.youtube.com/watch?v=TRDB2d61nDo", "Khan", );
+// }
+
+// let element = document.getElementById("kobayashi-maru");
+// element.addEventListener("onClick", playScenario);
